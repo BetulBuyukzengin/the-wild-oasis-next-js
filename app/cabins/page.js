5469dae0fbf5ed5594167802c2b2d83dbac14626
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-10-03 18:12:21 +0300

    feat: add metadata

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,9 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import Spinner from "../_components/Spinner";
 
+export const metadata = {
+  title: "Cabins",
+};
 export default function Cabins() {
   return (
     <div>
