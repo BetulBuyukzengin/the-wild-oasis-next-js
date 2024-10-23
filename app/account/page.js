f8ed611ce2339e3d2a6ab6b15745cfb9e0269a27
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-10-23 23:51:25 +0300

    feat: add user name from auth

--- app/account/page.js
+++ app/account/page.js
@@ -1,12 +1,16 @@
 //! Metadata
 
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
-function Page() {
+async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
   return (
     <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-      Welcome, Betül
+      Welcome, {firstName}
     </h2>
   );
 }
