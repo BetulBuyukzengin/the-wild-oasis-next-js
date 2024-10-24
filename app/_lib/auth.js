e18a258297b28e5686215e2f02b8d15ac2be9980
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-10-24 13:29:44 +0300

    feat: create a new guest on first sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -12,6 +13,24 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    //! Create new user if there is no existing user
+    async signIn({ user, account, profile }) {
+      try {
+        const existing = await getGuest(user.email);
+        if (!existing)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    //* We cannot use the session in signIn. Because the session has not been created yet. That's why we made it separate.
+    //! Create guest id
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   //! Redirect to a specially created login page
