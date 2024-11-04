f20323ac9f5dd57f4bc661c43eea18f79e20c42e
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2024-11-04 15:22:26 +0300

    feat: create reservation list, use optimistic hook for delete rezervation

---
+++ app/_components/ReservationList.js
@@ -0,0 +1,34 @@
+"use client";
+import { useOptimistic } from "react";
+import ReservationCard from "./ReservationCard";
+import { deleteReservation } from "../_lib/actions";
+
+function ReservationList({ bookings }) {
+  //! useOptimistic Hook
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    (curBookings, bookingId) => {
+      return curBookings.filter((booking) => booking.id !== bookingId);
+    }
+  );
+  async function handleDelete(bookingId) {
+    //! Delete reservation optimistic
+    optimisticDelete(bookingId);
+
+    //! Delete reservation from supabase
+    await deleteReservation(bookingId);
+  }
+  return (
+    <ul className="space-y-6">
+      {optimisticBookings.map((booking) => (
+        <ReservationCard
+          onDelete={handleDelete}
+          booking={booking}
+          key={booking.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default ReservationList;
